0d0f3951581c274b41c289553e35813f494acea9
Jason-Barron07 <jasonbarronyou222@gmail.com>
2023-06-02 14:51:40 +0200

    styling

---
+++ script/code.js
@@ -0,0 +1,9 @@
+const addtoList = document.querySelector("#addtoList");
+const sortList = document.querySelector("#sortingList");
+const displayArea = document.querySelector(".displayArea")
+const listItems = []
+
+
+
+// addtoList.addEventListener('click' )
+function addtoList ()
