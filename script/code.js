14cebe9bdc4b31d618f28b651f8a049a443b3fc9
Jason-Barron07 <jasonbarronyou222@gmail.com>
2023-06-02 16:32:02 +0200

    done

--- script/code.js
+++ script/code.js
@@ -7,9 +7,29 @@
 
 // addtoList.addEventListener('click' )
 
-
-
-
+document.querySelector('#add').onclick = function(){}
+    document.querySelector('#add').onclick = function(){
+        if(document.querySelector('#input').value.length == 0){}
+        else{
+            document.querySelector('#tasks').innerHTML += `
+        <div class="task">
+        <span id="sort-btn"></span>
+        <span id="taskname">
+        ${document.querySelector('#input').value}
+        </span>
+        <button class="delete">
+        <i class="far fa-trash-alt">delete</i>
+        </button>
+        </div>
+            `;
+            let current_tasks = document.querySelectorAll(".delete");
+            for(let i=0; i<current_tasks.length; i++){
+                current_tasks[i].onclick = function(){
+                    this.parentNode.remove();}
+    }
+        }
+      };
+    
 
 
 
